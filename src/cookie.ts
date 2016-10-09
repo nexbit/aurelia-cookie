c9c0f81ff464d726c90508014e01a7b9cbb1f62c
Paolo Furini <paolo.furini@gmail.com>
2016-10-09 17:16:08 +0000

    fix(options): make all options members optional and correct types

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,9 +1,9 @@
 export interface OptionsInterface {
-    expires: any;
+    expires?: Date;
     expiry?: number;
-    path: string;
-    domain: string;
-    secure: string;
+    path?: string;
+    domain?: string;
+    secure?: boolean;
 }
 
 export class Cookie {
